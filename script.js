45671acaed0161e9a3136c820f17e5efbb05174e
Lorrayne <lorraynelimamota@gmail.com>
2023-12-27 00:21:13 -0300

    não lembro a alteração kkkk

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
+let menuAnimated = document.querySelector(".menuAnimated");
+
+function myFunction() {
+    this.classList.toggle("change");
+}
+
+menuAnimated.addEventListener("click", myFunction);
+
+let newTab = document.querySelector (".newTab");
+
+function fly(){
+  newTab.classList.toggle("show")
+}
+
+menuAnimated.addEventListener("click", fly)
